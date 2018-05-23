c25e60498ea73ad9ae387b98c93ef4d8c2b27796
Gabor Szabo <gabor@szabgab.com>
2018-05-23 16:21:21 +0300

    points

--- python-mocking/classy/application.py
+++ python-mocking/classy/application.py
@@ -1,13 +1,28 @@
+class Line(object):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-class Data(object):
-    def get_info(self, issue):
-    
-    def __init__(self, errors, warnings):
-        self.errors = errors
-        self.warnings = warnings
+    def distance(self):
+        return ((self.a.x - self.b.x) ** 2  + (self.a.y - self.b.y) ** 2) ** 0.5
 
-   
+class Point(object):
+    def __init__(self, x , y):
+        self.x = x
+        self.y = y
 
-def get_data(issue):
-        # connnect to database, fetch data
-    Data(issue)
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+p1 = Point(0, 0)
+p2 = Point(0, 2)
+l = Line(p1, p2)
+print(l.distance())
+
+p2.move(0, 4)
+print(l.distance())
+
+p2.move(3, -2)
+print(l.distance())
+
