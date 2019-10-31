1355c9a526010c9a2c22d69e7307de5e2b67b4f2
Gabor Szabo <gabor@szabgab.com>
2019-10-31 07:43:19 +0200

    save image

--- python/examples/seaborn/tips.py
+++ python/examples/seaborn/tips.py
@@ -3,11 +3,12 @@ Source : https://seaborn.pydata.org/introduction.html
 """
 
 import seaborn as sns
-import matplotlib
 
 sns.set()
-tips = sns.load_dataset("tips")
-r = sns.relplot(
+tips = sns.load_dataset("tips")  # Reads into Pandas DataFrame
+#print(type(tips))
+
+plot = sns.relplot(
     x="total_bill",
     y="tip",
     col="time",
@@ -15,4 +16,7 @@ r = sns.relplot(
     style="smoker",
     size="size",
     data=tips)
-matplotlib.pyplot.show(r)
+#print(type(plot))
+plot.savefig("out.png")
+#matplotlib.pyplot.show(r)
+#matplotlib.pyplot.savefig(r)
