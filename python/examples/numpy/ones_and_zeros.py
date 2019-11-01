fc008620334e3b842e3a5c74638f1d8d81870d2a
Gabor Szabo <gabor@szabgab.com>
2019-11-01 06:23:16 +0200

    more

--- python/examples/numpy/ones_and_zeros.py
+++ python/examples/numpy/ones_and_zeros.py
@@ -10,3 +10,10 @@ d = np.zeros(3, dtype='float32')
 print(d)          # [ 0.  0.  0.]
 print(d.dtype)    # float32
 print(d.shape)    # (3,)
+
+a = np.ones([2, 3])
+print(a)
+# [[1., 1., 1.],
+# [1., 1., 1.]]
+print(a.dtype)    # float64
+print(a.shape)    # (2, 3)
