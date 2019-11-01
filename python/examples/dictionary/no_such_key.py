f9cf7728cc6e04a9e4c526cb05fcb7d1d15740ca
Gabor Szabo <gabor@szabgab.com>
2019-11-01 06:59:14 +0200

    main

--- python/examples/dictionary/no_such_key.py
+++ python/examples/dictionary/no_such_key.py
@@ -1,13 +1,17 @@
-user = {
-    'fname': 'Foo',
-    'lname': 'Bar',
-}
 
-print(user['fname'])
-print(user['email'])
+def main():
+    user = {
+        'fname': 'Foo',
+        'lname': 'Bar',
+    }
+
+    print(user['fname'])
+    print(user['email'])
 
 # Foo
 # Traceback (most recent call last):
 #   File "examples/dictionary/no_such_keys.py", line 9, in <module>
 #     print user['email']
 # KeyError: 'email'
+
+main()
