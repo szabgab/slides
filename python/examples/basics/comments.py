5755640a122226f0c2c69f0cd7c3af416c3f1c57
Gabor Szabo <gabor@szabgab.com>
2019-11-08 11:13:47 +0200

    python

--- python/examples/basics/comments.py
+++ python/examples/basics/comments.py
@@ -2,13 +2,5 @@ print("hello")
 
 # Comments for other developers
 
-'A string which is disregarded'
+print("world") # more comments
 
-print(42) # the answer
-
-'''
-  Using three single-quotes on both ends (a triple-quoted string)
-  can be used as a multi-line comment.
-'''
-
-print("world")
---
+++ python/examples/basics/string_comments.py
@@ -0,0 +1,12 @@
+print("hello")
+
+'A string which is disregarded'
+
+print(42)
+
+'''
+  Using three single-quotes on both ends (a triple-quoted string)
+  can be used as a multi-line comment.
+'''
+
+print("world")
