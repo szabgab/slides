fd7c589b4378caa9dc83f73afe5e36745ec3405f
Gabor Szabo <gabor@szabgab.com>
2019-11-13 14:53:24 +0200

    add modules

--- python/examples/basics/cli.py
+++ python/examples/basics/cli.py
@@ -1,12 +1,9 @@
 import sys
 
-
 def main():
     print(sys.argv)
     print(sys.argv[0])
     print(sys.argv[1])
     print(sys.argv[2])
 
-
 main()
-
--- python/examples/basics/prompt.py
+++ python/examples/basics/prompt.py
@@ -2,10 +2,10 @@ from __future__ import print_function
 import sys
 
 def main():
-  if sys.version_info.major < 3:
-      name = raw_input('Your name: ')
-  else: 
-      name = input('Your name: ')
-  print('Hello ' + name + ', how are you?')
+    if sys.version_info.major < 3:
+        name = raw_input('Your name: ')
+    else:
+        name = input('Your name: ')
+    print('Hello ' + name + ', how are you?')
 
 main()
