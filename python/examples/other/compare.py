d9c8b835c9bb2b079cbd7f362562e91cb133042d
Gabor Szabo <gabor@szabgab.com>
2019-11-14 19:48:25 +0200

    don't show parallel assignment here

--- python/examples/other/compare.py
+++ python/examples/other/compare.py
@@ -1,13 +1,16 @@
-x =12
+x = 12
 y = 3
 print(x > y)     # True
 
-x = '12'; y = 3
+x = '12'
+y = 3
 print(x > y)     # True
 
-x, y = "12", "3"
+x = "12"
+y = "3"
 print(x > y)      # False
 
-x, y = 12, '3'
+x = 12
+y = '3'
 print(x > y)      # False
 
