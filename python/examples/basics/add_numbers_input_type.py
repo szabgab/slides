a795a9d4b1727deee67e87f3143d96a80b4f30b8
Gabor Szabo <gabor@szabgab.com>
2019-11-14 09:37:00 +0200

    another examples

---
+++ python/examples/basics/add_numbers_input_type.py
@@ -0,0 +1,21 @@
+def main():
+    a = input('First number: ')
+    b = input('Second number: ')
+    print( type(a) )
+    print(int(a) + int(b))
+    # a1 = int(a)
+    # b1 = int(b)
+    # print( type(a) )
+    # print( type(a1) )
+    # c = a1 + b1
+    # print(c)
+    # print( type(c) )
+    
+    a = int(a)
+    b = int(b)
+    print( type(a) )
+    print(a + b)
+    
+
+main()
+
