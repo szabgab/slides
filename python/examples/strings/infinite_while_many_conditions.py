cd600075796057a30233c5e1b86e659b79e031e6
Gabor Szabo <gabor@szabgab.com>
2019-11-20 18:35:39 +0200

    updates

--- python/examples/strings/infinite_while_many_conditions.py
+++ python/examples/strings/infinite_while_many_conditions.py
@@ -1,10 +1,16 @@
 
-
 while True:
+   line = get_next_line()
+   
+   if last_line:
+       break
+   
    if line is empty:
       continue
+
    if line has an # at the  beginning:
       continue
+
    if line has a // at the beginning:
       continue
 
---
+++ python/examples/strings/while_many_conditions.py
@@ -0,0 +1,17 @@
+
+while (not found_error) and (not found_warning) and (not found_exit):
+   do the real stuff
+
+while True:
+    line = get_next_line()
+
+    if found_error:
+        break
+
+    if found_warning:
+        break
+
+    if found_exit:
+        break
+
+   do the real stuff
