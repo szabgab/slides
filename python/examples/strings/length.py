5ecf18ec22181ee7ca4bc487fd27a67c873c16e3
Gabor Szabo <gabor@szabgab.com>
2019-11-20 10:44:37 +0200

    examples

---
+++ python/examples/strings/length.py
@@ -0,0 +1,6 @@
+line = "Hello World"
+print(len(line)) # 11
+
+text = """Hello 
+World"""
+print(len(text))
--- python/examples/strings/quotes.py
+++ python/examples/strings/quotes.py
@@ -4,3 +4,15 @@ salad = 'Ceasar salad'
 print(soup)
 print(salad)
 
+z = "abc" "def"
+print(z)  # abcdef
+
+q = "one" \
+    "two" \
+    "three"
+print(q)  # onetwothree
+
+text = "first row \
+second row \
+third row"
+print(text)
