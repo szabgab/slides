a8a14b4c860ac0ccb5230d18c2e7334012a6abf8
Gabor Szabo <gabor@szabgab.com>
2019-11-20 08:10:19 +0200

    cli

--- python/examples/basics/cli_exit_3.py
+++ python/examples/basics/cli_exit_3.py
@@ -2,7 +2,8 @@ import sys
 
 def main():
     if len(sys.argv) != 2:
-        exit("Usage: " + sys.argv[0] + " VALUE")
+        print("Usage: " + sys.argv[0] + " VALUE")
+        exit(3)
     print("Hello " + sys.argv[1])
 
 main()
