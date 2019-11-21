3e0faaf4473cd575191800f839a86a8061a8d5ff
Gabor Szabo <gabor@szabgab.com>
2019-11-21 20:38:45 +0200

    update

--- golang/examples/json/json-round-trip.go
+++ golang/examples/json/json-round-trip.go
@@ -9,7 +9,8 @@ func main() {
     var person = map[string]string{}
     person["name"] = "Foo Bar"
     person["email"] = "foo@bar.com"
-    fmt.Println(person)
-    fmt.Println(json.Marshal(person))
+    //fmt.Println(person)
+    mystr, _ := json.Marshal(person)
+    fmt.Printf("%T\n", mystr)
+    //fmt.Printf("%s", json.Marshal(person))
 }
-
