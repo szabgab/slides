758c5d66495be0abeaed683eb8eaba1646cb5ab3
Gabor Szabo <gabor@szabgab.com>
2019-11-22 16:10:17 +0200

    separate out the copy funciton

--- combine.py
+++ combine.py
@@ -11,6 +11,21 @@ def get_arguments():
     parser.add_argument('--all', action='store_true', help='collect all the slides')
     return parser.parse_args()
 
+def process_slides(source, resources, name):
+    logging.info(f"Processing slides of {name}")
+    src_examples = os.path.join(source, name, 'examples')
+    if os.path.exists(src_examples):
+        for example in os.listdir(src_examples):
+            #print(example)
+            example_path = os.path.join(src_examples, example)
+            if os.path.isdir(example_path):
+                logging.debug(f"Copy dir {example_path}")
+                shutil.copytree(example_path, os.path.join(resources, 'examples', example))
+            else:
+                logging.error(f"{example_path}")
+                # TODO maybe I should not even handle this just let the code blow up so I will be forced to fix
+                # this
+
 
 def main():
     logging.basicConfig(
@@ -42,25 +57,14 @@ def main():
     os.mkdir(target)
     os.mkdir(resources)
 
-    slides_file = os.path.join(source, 'slides.txt')
-    with open(slides_file, 'r') as fh:
-        for line in fh:
-            line = line.rstrip("\n")
-            #if line != "docker":
-            #    continue
-            logging.info(f"Processing slides of {line}")
-            src_examples = os.path.join(source, line, 'examples')
-            if os.path.exists(src_examples):
-                for example in os.listdir(src_examples):
-                    #print(example)
-                    example_path = os.path.join(src_examples, example)
-                    if os.path.isdir(example_path):
-                        logging.debug(f"Copy dir {example_path}")
-                        shutil.copytree(example_path, os.path.join(resources, 'examples', example))
-                    else:
-                        logging.error(f"{example_path}")
-                        # TODO maybe I should not even handle this just let the code blow up so I will be forced to fix
-                        # this
+    if args.all:
+        slides_file = os.path.join(source, 'slides.txt')
+        with open(slides_file, 'r') as fh:
+            for line in fh:
+                line = line.rstrip("\n")
+                process_slides(source, resources, line)
+    else:
+        process_slides(source, resources, args.name)
 
 main()
 
