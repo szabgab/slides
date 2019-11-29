c1c31b9e9736a87bd6b2a4a55901f7cd87bbd358
Gabor Szabo <gabor@szabgab.com>
2019-11-29 08:00:42 +0200

    convert to functions

--- generate_slides.py
+++ generate_slides.py
@@ -14,49 +14,59 @@ parent = os.path.dirname(root)
 print(root)
 print(parent)
 
-html_root = os.path.join(root, 'html')
 
-print(html_root)
-if os.path.exists(html_root):
-    shutil.rmtree(html_root)
-os.mkdir(html_root)
+def main():
+    html_root = os.path.join(root, 'html')
 
-# ext=""
-# if [ "$1" == "--ext" ]
-# then
-#     ext="--ext $2"
-#     shift
-#     shift
-# fi
-ext = "--ext html"
-with open(os.path.join(root, 'slides.txt')) as fh:
-    names = map(lambda s: s.rstrip("\n"), fh.readlines())
+    print(html_root)
+    if os.path.exists(html_root):
+        shutil.rmtree(html_root)
+    os.mkdir(html_root)
 
-with open(os.path.join(root, 'books.txt')) as fh:
-    books = dict(map(lambda x: x.split('/'), map(lambda s: s.rstrip("\n"), fh.readlines())))
+    # ext=""
+    # if [ "$1" == "--ext" ]
+    # then
+    #     ext="--ext $2"
+    #     shift
+    #     shift
+    # fi
+    ext = "--ext html"
+    with open(os.path.join(root, 'slides.txt')) as fh:
+        names = map(lambda s: s.rstrip("\n"), fh.readlines())
+
+    with open(os.path.join(root, 'books.txt')) as fh:
+        books = dict(map(lambda x: x.split('/'), map(lambda s: s.rstrip("\n"), fh.readlines())))
 
 
-if len(sys.argv) == 2:
-    only_one = sys.argv[1]
-    if only_one in names:
-        names = [only_name]
-        books = {}
-    elif only_name in books:
-        names = []
-        books = { only_name :  books[only_name] }
+    if len(sys.argv) == 2:
+        only_one = sys.argv[1]
+        if only_one in names:
+            names = [only_name]
+            books = {}
+        elif only_name in books:
+            names = []
+            books = { only_name :  books[only_name] }
 
-for name in names:
-    print(name)
-    cmd = f"{sys.executable} {parent}/slider-py/slider.py --md {root}/{name}/intro.md --html --dir {root}/html/{name}/ --templates {root}/templates/ --static {root}/static/ {ext}"
-    ret = os.system(cmd)
-    if ret != 0:
-        exit("Failed")
+    generate_singles(names, ext)
+    generate_multis(books, ext)
 
-#for (name, filename) in books.items():
-#    print(f"{name} - {filename}")
-#    cmd = f"{sys.executable} {parent}/slider-py/slider.py --yaml {root}/{name}/{filename} --html --dir {root}/html/{name}/ --templates {root}/templates/ --static {root}/static/ {ext}"
-#    ret = os.system(cmd)
-#    if ret != 0:
-#        exit("Failed")
+def generate_singles(names, ext):
+    for name in names:
+        print(name)
+        cmd = f"{sys.executable} {parent}/slider-py/slider.py --md {root}/{name}/intro.md --html --dir {root}/html/{name}/ --templates {root}/templates/ --static {root}/static/ {ext}"
+        ret = os.system(cmd)
+        if ret != 0:
+            exit("Failed")
+
+def generate_multis(books, ext):
+    for (name, filename) in books.items():
+        print(f"{name} - {filename}")
+        cmd = f"{sys.executable} {parent}/slider-py/slider.py --yaml {root}/{name}/{filename} --html --dir {root}/html/{name}/ --templates {root}/templates/ --static {root}/static/ {ext}"
+        ret = os.system(cmd)
+        if ret != 0:
+            exit("Failed")
+
+
+main()
 
 
