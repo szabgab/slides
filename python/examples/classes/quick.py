ced996c7b157e49f25e22a17f03575a73f5356f6
Gabor Szabo <gabor@szabgab.com>
2019-12-04 07:26:35 +0200

    examples

---
+++ python/examples/classes/quick.py
@@ -0,0 +1,9 @@
+class Quick(object):
+   def __init__(self, name, email):
+       self.name = name
+       self.email = email
+
+q = Quick(name = "Foo", email = "foo@bar.com")
+print(q.name)
+print(q.email)
+
