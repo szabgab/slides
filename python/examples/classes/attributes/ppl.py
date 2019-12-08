ee4bfc8cb1a9aef8081f9d70485aeda931d5e6af
Gabor Szabo <gabor@szabgab.com>
2019-12-08 22:11:08 +0200

    examples

---
+++ python/examples/classes/attributes/ppl.py
@@ -0,0 +1,17 @@
+class Person(object):
+    def __init__(self, given_name):
+        self.name = given_name
+
+if __name__ == '__main__':
+    p1 = Person("Joe")
+    print(p1.__class__.__name__)  # Person
+    print(p1.name)                # Joe
+
+    p2 = Person("Jane")
+    print(p2.name)                # Jane
+
+    p1.address = "Main street 12"
+    print(p1.address)             # Main street 12
+
+
+    print(p2.address)             # AttributeError: 'Person' object has no attribute 'address'
--- python/examples/classes/methods/point.py
+++ python/examples/classes/methods/point.py
@@ -9,3 +9,5 @@ p1.move(4, 5)
 print(p1.x)    # 6
 print(p1.y)    # 8
 
+
+print(p1)      # <shapes.Point object at 0x7fb0691c3e48>
