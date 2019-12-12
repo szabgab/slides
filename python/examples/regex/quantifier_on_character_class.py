b66cf958f79463161e63d2019c4667590989ee84
Gabor Szabo <gabor@szabgab.com>
2019-12-12 20:39:00 +0200

    improve

--- python/examples/regex/quantifier_on_character_class.py
+++ python/examples/regex/quantifier_on_character_class.py
@@ -13,12 +13,13 @@ for line in strings:
     match = re.search(r'-[abc]-', line)
     if match:
         print(line)
-
+print('=========================')
 
 for line in strings:
     match = re.search(r'-[abc]+-', line)
     if match:
         print(line)
+print('=========================')
 
 for line in strings:
     match = re.search(r'-[abc]*-', line)
