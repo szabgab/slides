5c1056f01eb344b3888e36665856c2df84540498
Gabor Szabo <gabor@szabgab.com>
2019-12-21 11:03:55 +0200

    more parsing

--- python/examples/oop/parser/parser.py
+++ python/examples/oop/parser/parser.py
@@ -7,27 +7,29 @@ def main():
     if len(sys.argv) != 2:
         exit(f'Usage: {sys.argv[0]} FILENAME')
     dom = parse(sys.argv[1])
-    print(json.dumps(dom))
+    print(json.dumps(dom, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False))
 
 def parse(filename):
     dom = []
     with open(filename) as fh:
+        text = ''
         for line in fh:
             line = line.rstrip('\n')
 
-            match = re.search(r'\A#\s+(.*?)\s*\Z', line)
+            match = re.search(r'\A(#{1,2})\s+(.*?)\s*\Z', line)
             if match:
+                if text != '':
+                    dom.append({
+                        'p': text
+                    })
+                    text = ''
+                h = 'h' + str( len(match.group(1)) )
                 dom.append({
-                    'h1' : match.group(1)
+                    h : match.group(2)
                 })
                 continue
 
-            match = re.search(r'\A##\s+(.*?)\s*', line)
-            if match:
-                dom.append({
-                    'h2' : match.group(1)
-                })
-                continue
+            text += line + '\n'
 
     return dom
 
