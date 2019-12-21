5fa2bf3cc3542dc27abd56efaca3be364f4e91cc
Gabor Szabo <gabor@szabgab.com>
2019-12-21 21:02:56 +0200

    more

--- python/examples/lists/simple_generator.py
+++ python/examples/lists/simple_generator.py
@@ -1,5 +1,13 @@
 numbers  = [0, 1, 2, 3]
 
 gn = (n*n for n in numbers)
-print(gn)   # <generator object <genexpr> at 0x7f9903dbc930>
-print(list(gn)) # [0, 1, 4, 9]
+print(gn)
+
+for num in gn:
+    print(num)
+
+
+gn = (n*n for n in numbers)
+print(list(gn))
+
+print(list(gn))
