77bfa5f369e8563750504bb0f555034487f1e218
Gabor Szabo <gabor@szabgab.com>
2020-01-05 14:50:17 +0200

    attribute

--- python-programming/examples/memoize/memoize_attribute.py
+++ python-programming/examples/memoize/memoize_attribute.py
@@ -1,11 +1,12 @@
 def memoize(f):
     def caching(n):
         key = n
-        if 'data' not in caching.__dict__:
-            caching.data = {}
+        #if 'data' not in caching.__dict__:
+        #    caching.data = {}
         if key not in caching.data:
             caching.data[key] = f(n)
         return caching.data[key]
+    caching.data = {}
 
     return caching
 
