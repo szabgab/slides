75fcf26a49bf6810ac1857a7949323167602fe0a
Gabor Szabo <gabor@szabgab.com>
2020-01-05 13:55:56 +0200

    elapsed time

--- python-programming/examples/advanced/logger_decor.py
+++ python-programming/examples/advanced/logger_decor.py
@@ -1,7 +1,11 @@
+import time
 def tron(func):
     def new_func(*args, **kwargs):
+        start = time.time()
         print("Calling {}({}, {})".format(func.__name__, args, kwargs))
         out = func(*args, **kwargs)
+        end = time.time()
         print("Finished {}({})".format(func.__name__, out))
+        print("Elapsed time: {}".format(end - start))
         return out
     return new_func
