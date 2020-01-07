86054db0cce96278383cf88a4cc834a4959eba65
Gabor Szabo <gabor@szabgab.com>
2020-01-07 15:47:01 +0200

    fix

--- python-programming/examples/files/open_file_handle.py
+++ python-programming/examples/files/open_file_handle.py
@@ -3,7 +3,9 @@ filename = 'examples/files/unicorns.txt'
 try:
     with open(filename, 'r') as fh:
         lines = fh.read()
-except Exception:
+except Exception as err:
     print('There was some error in the file operations.')
+    print(err)
+    print(type(err).__name__)
 
 print('Still running.')
