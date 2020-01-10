371c0dc473b2af5eff877daf11958002f45d8df2
Gabor Szabo <gabor@szabgab.com>
2020-01-10 14:36:56 +0200

    c# examples

--- python-programming/examples/dotnet/math/MyMath.cs
+++ python-programming/examples/dotnet/math/MyMath.cs
@@ -6,5 +6,30 @@ namespace MyMath
         {
             return a+b;
         }
+
+        public static double addDouble(double a, double b)
+        {
+            return a+b;
+        }
+
+        public static string addString(string a, string b)
+        {
+            return a+" "+b;
+        }
+
+        public static bool andBool(bool a, bool b)
+        {
+            return a && b;
+        }
+
+        public static string str_by_index(string[] a, int b)
+        {
+            return a[b];
+        }
+        public static int int_by_index(int[] a, int b)
+        {
+            return a[b];
+        }
+
     }
 }
--- python-programming/examples/dotnet/math/myapp.py
+++ python-programming/examples/dotnet/math/myapp.py
@@ -4,4 +4,28 @@ assembly = clr.AddReference('MyMath')
 #print(type(assembly)) # <class 'System.Reflection.RuntimeAssembly'>
 #print(dir(assembly))
 from MyMath import MyMathClass
-print(MyMathClass.addInts(2, 3))
+from MyMath import MyMathClass as My
+
+
+assert My.addInts(2, 3)         == 5
+assert My.addInts(2.7, 7.8)     == 9
+assert My.addDouble(11.2, 23.3) == 34.5
+assert My.addString("hello", "world") == "hello world"
+
+assert My.andBool(1, 1) is True
+assert My.andBool(1, 0) is False
+assert My.andBool(True, True) is True
+assert My.andBool(False, True) is False
+
+assert My.str_by_index(["apple", "banana", "peach"], 0) == "apple"
+assert My.str_by_index(["apple", "banana", "peach"], 1) == "banana"
+assert My.int_by_index([17, 19, 42], 1) == 19
+# Mixed list cannot be passed
+
+# tuple can be passed
+assert My.int_by_index((17, 21, 42), 2) == 42
+
+# TODO: string, char, float
+# TODO strings, lists, dicts,
+# TODO complex data structures in C#
+# TODO Async
