5f1a26b280761ac7c4b60702629b211cced77e6a
Gabor Szabo <gabor@szabgab.com>
2020-02-12 15:06:35 +0200

    update

--- python-programming/examples/flask/15/app.py
+++ python-programming/examples/flask/15/app.py
@@ -7,9 +7,9 @@ def main():
 Main<br>
 <a href="/user/23">23</a><br>
 <a href="/user/42">42</a><br>
+<a href="/user/Joe">Joe</a><br>
 '''
 
 @app.route("/user/<uid>")
 def api_info(uid):
     return uid
-
