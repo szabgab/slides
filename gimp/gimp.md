b39557ead3c21c19202dc633059304c603d3130c
Gabor Szabo <gabor@szabgab.com>
2020-03-07 08:22:56 +0200

    extra examples

---
+++ gimp/gimp.md
@@ -0,0 +1,13 @@
+# Gimp
+{id: gimp}
+
+## Steps
+{id: steps}
+
+Create new image
+* File / New  (size in pixels)
+
+Text
+T -
+Alt-drag to move the text box (text layer)
+
---
+++ nodejs/examples/web-express-2/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World 2!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
