641f470ca660a2dbd6ab0f0ce4730ad40ba43b5a
Gabor Szabo <gabor@szabgab.com>
2020-03-17 17:53:19 +0200

    fix

--- python-programming/examples/lists/colors.py
+++ python-programming/examples/lists/colors.py
@@ -2,5 +2,5 @@ colors = ['blue', 'yellow', 'black', 'purple']
 for i in range(len(colors)):
     print("{}) {}".format(i+1, colors[i]))
 
-c = int(raw_input("Select color ")) - 1
+c = int(input("Select color ")) - 1
 print(colors[c])
