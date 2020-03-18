b7276c2c5bcf72cffa90ad517f675bcf337b2503
Gabor Szabo <gabor@szabgab.com>
2020-03-18 12:42:02 +0200

    explain count characters solution

--- python-programming/examples/dictionary/count_characters.py
+++ python-programming/examples/dictionary/count_characters.py
@@ -3,14 +3,16 @@ This is a very long text.
 OK, maybe it is not that long after all.
 """
 
-# print text
+# print(text)
 count = {}
-for c in text:
-    if c == '\n':
-        continue
-    if c not in count:
-        count[c] = 0
-    count[c] += 1
 
-for k in count:
-    print("'{}' {}".format(k, count[k]))
+for char in text:
+    if char == '\n':
+        continue
+    if char not in count:
+        count[char] = 1
+    else:
+        count[char] += 1
+
+for key in sorted( count.keys() ):
+    print("'{}' {}".format(key, count[key]))
