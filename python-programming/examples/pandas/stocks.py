e23267cf71e1663d91d5c8446d956ca41b452110
Gabor Szabo <gabor@szabgab.com>
2020-03-19 07:57:17 +0200

    fix

--- python-programming/examples/pandas/stocks.py
+++ python-programming/examples/pandas/stocks.py
@@ -1,19 +1,15 @@
 import pandas
 import pandas_datareader.data as web
-all_data = {ticker: web.get_data_yahoo(ticker)
-            for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']}
+all_data = { ticker: web.get_data_yahoo(ticker) for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']}
 
 print(all_data.keys())          # dict_keys(['MSFT', 'IBM', 'AAPL', 'GOOG'])
-print(all_data['MSFT'].keys())
-   # Index(['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'], dtype='object')
+print(all_data['MSFT'].keys())  # Index(['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'], dtype='object')
 
-price = pandas.DataFrame({ticker: data['Adj Close']
-                         for ticker, data in all_data.items()})
+price = pandas.DataFrame({ticker: data['Adj Close'] for ticker, data in all_data.items()})
 
 print(price.head())
 
-volume = pandas.DataFrame({ticker: data['Volume']
-                          for ticker, data in all_data.items()})
+volume = pandas.DataFrame({ticker: data['Volume'] for ticker, data in all_data.items()})
 
 print(volume.tail())
 
