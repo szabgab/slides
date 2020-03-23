c57e600543cb4a3723cb610ba651d796b523793d
Gabor Szabo <gabor@szabgab.com>
2020-03-23 17:13:28 +0200

    more

--- golang/examples/list_directory/listdir.go
+++ golang/examples/list_directory/listdir.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-	//"io/ioutil"
 )
 
 func main() {
-	//files, err := ioutil.ReadDir
 	fmt.Println("hello")
 	fmt.Println(os.Args)
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	}
+
+	fmt.Println(os.Args[1])
+	files, err := ioutil.ReadDir(".") //os.Args[1])
+	if err == nil {
+		fmt.Println("Error reading the directory list")
+		log.Fatal(err)
+	} else {
+		fmt.Println(files)
+	}
 }
