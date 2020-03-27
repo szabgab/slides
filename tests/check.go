bee5ce8cd0d4e87b3178f34b12a53a9049e48247
Gabor Szabo <gabor@szabgab.com>
2020-03-27 07:31:49 +0300

    travers the directory tree

--- tests/check.go
+++ tests/check.go
@@ -2,8 +2,31 @@ package main
 
 import (
     "fmt"
+    "os"
+    "path/filepath"
 )
 
 func main() {
     fmt.Println("Checking the Go slides")
+    walk()
+}
+
+
+func walk() {
+    root := "golang"
+    subDirToSkip := "xyz"
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            fmt.Printf("Cannot access dir at path %q: %v", path, err)
+            return err
+        }
+        if info.IsDir() && info.Name() == subDirToSkip {
+            fmt.Printf("Skipping directory %+v \n", info.Name())
+        }
+        fmt.Printf("Visiting %q\n", path)
+        return nil
+    })
+    if err != nil {
+        fmt.Printf("error walking the path %q: %v\n", root, err)
+    }
 }
