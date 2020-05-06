7ecbfa012129546499c6dc2b2cfe951e196c09d7
Gabor Szabo <gabor@szabgab.com>
2020-05-06 09:00:10 +0300

    fix text

--- python-programming/examples/tk/simple_file_dialog.py
+++ python-programming/examples/tk/simple_file_dialog.py
@@ -3,4 +3,4 @@ from tkinter import filedialog
 input_file_path = filedialog.askopenfilename(filetypes=(("Excel files", "*.xlsx"), ("CSV files", "*.csv"), ("Any file", "*")))
 print(input_file_path)
 
-input("Press any key...")
+input("Press ENTER to end the script...")
