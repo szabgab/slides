de7a2140f438878e069cb3b7e48fbb0c34173be0
Gabor Szabo <gabor@szabgab.com>
2020-06-08 18:51:27 +0300

    make it work

--- docker/examples/python-pulsar/app.py
+++ docker/examples/python-pulsar/app.py
@@ -4,6 +4,11 @@ import pulsar
 
 app = Flask(__name__)
 
+topic = "name"
+
+client = pulsar.Client('pulsar://my-pulsar:6650')
+consumer = client.subscribe(topic, 'my-subscription')
+producer = client.create_producer(topic)
 
 
 @app.route("/")
@@ -14,23 +19,16 @@ def main():
 
 @app.route("/save", methods=['POST'])
 def save():
-    topic = request.form['topic']
     value = request.form['value']
-    app.logger.info(f"to save: {topic} - {value}")
+    app.logger.info(f"to save: {value}")
 
-    client = pulsar.Client('pulsar://my-pulsar:6650')
-    producer = client.create_producer(topic)
-    res = producer.send(value.encode('utf-8'))
-    app.logger.info(f"result: {res}")
-    client.close()
-    return render_template('main.html', saved=1, value=f"{topic}: {value}")
+    producer.send(value.encode('utf-8'))
+    app.logger.info("sent message")
+    return render_template('main.html', saved=1, value=value)
 
 @app.route("/get", methods=['POST'])
 def get():
-    topic = request.form['topic']
 
-    client = pulsar.Client('pulsar://my-pulsar:6650')
-    consumer = client.subscribe(topic, 'my-subscription')
     app.logger.info("consumer")
     msg = consumer.receive()
     app.logger.info("received")
@@ -39,7 +37,6 @@ def get():
         consumer.acknowledge(msg)
     except Exception as err:
         value = f"Exception {err}"
-    client.close()
     app.logger.info(f"value: {value}")
 
-    return render_template('main.html', field=field, value=value)
+    return render_template('main.html', value=value)
