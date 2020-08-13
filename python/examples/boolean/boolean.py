f4f0c7c5691dc2600b7281dde05160a538bc057e
Gabor Szabo <gabor@szabgab.com>
2020-08-13 08:57:45 +0300

    fix

--- python/examples/boolean/boolean.py
+++ python/examples/boolean/boolean.py
@@ -3,8 +3,16 @@ x = 23
 if x:
     print("23 is true")
 
+if x != 0:
+    print("23 is true")
+
 y = 0
 if y:
+    print("0 is true")
+else:
+    print("0 is false")
+
+if y != 0:
     print("0 is true")
 else:
     print("0 is false")
