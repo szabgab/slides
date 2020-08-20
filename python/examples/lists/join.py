b9839df2104be2f408bf65bc2ccd6088e81f3ce5
Gabor Szabo <gabor@szabgab.com>
2020-08-20 17:02:45 +0300

    update

--- python/examples/lists/join.py
+++ python/examples/lists/join.py
@@ -3,6 +3,9 @@ fields = ['one', 'two and three', 'four', 'five']
 together = ':'.join(fields)
 print(together) # one:two and three:four:five
 
+together = ' '.join(fields)
+print(together) # one two and three four five
+
 mixed = ' -=<> '.join(fields)
 print(mixed) # one -=<> two and three -=<> four -=<> five
 
--- python/examples/lists/split.py
+++ python/examples/lists/split.py
@@ -1,10 +1,10 @@
-words = "ab:cd:ef".split(':')
-print(words)   # ['ab', 'cd', 'ef']
+words = "ab:cd::ef".split(':')
+print(words)   # ['ab', 'cd', '', 'ef']
 
 # special case: split by spaces
 names = "foo   bar baz".split()
 print(names)   # ['foo', 'bar', 'baz']
 
 # special case: split to characters
-chars = list("abcd")
-print(chars)   # ['a', 'b', 'c', 'd']
+chars = list("ab cd")
+print(chars)   # ['a', 'b', ' ', 'c', 'd']
