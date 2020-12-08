7100d602f852b574662bd72aa6bfbed7b7f57ead
Gabor Szabo <gabor@szabgab.com>
2020-12-08 18:45:51 +0200

    relative path

--- generate_slides.py
+++ generate_slides.py
@@ -13,7 +13,7 @@ import datetime
 root   = os.path.dirname(os.path.abspath(__file__))
 parent = os.path.dirname(root)
 slider = "{}/slider-py/slider.py".format(parent)
-if os.environ.get('TRAVIS'):
+if os.environ.get('TRAVIS') or os.environ.get('GITHUB_WORKFLOW'):
     slider = "{}/slider-py/slider.py".format(root)
 #print("root={}".format(root))
 #print("parent={}".format(parent))
