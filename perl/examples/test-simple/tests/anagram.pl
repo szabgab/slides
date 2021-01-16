d34e2beb982a7dac67e4d72af701d902d9404055
Gabor Szabo <gabor@szabgab.com>
2021-01-16 20:54:31 +0200

    update

--- perl/examples/test-simple/tests/anagram.pl
+++ perl/examples/test-simple/tests/anagram.pl
@@ -5,7 +5,7 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use MyAnagram qw(is_anagram);
 
-use Test::Simple tests => 4;
+use Test::Simple tests => 5;
 
 ok( is_anagram("abc", "abc") );
 ok( is_anagram("silent", "listen") );
@@ -13,3 +13,4 @@ ok( is_anagram("silent", "listen") );
 ok( not is_anagram("abc", "abd") );
 
 ok( is_anagram("anagram", "nag a gram") );
+ok( is_anagram("ABC", "abc") );
