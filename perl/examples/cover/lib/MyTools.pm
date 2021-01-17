c796acabe02bb12d6a9b47d30fda78c331d0f1a4
Gabor Szabo <gabor@szabgab.com>
2021-01-17 17:27:26 +0200

    add example for coverage

---
+++ perl/examples/cover/lib/MyTools.pm
@@ -0,0 +1,41 @@
+package MyTools;
+use strict;
+use warnings;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(add div fibo);
+
+our $VERSION = '0.02';
+
+=head1 NAME
+
+MyTools - some tools to show packaging
+
+=head1 SYNOPSIS
+
+Short example
+
+=cut
+
+sub add {
+	return $_[0] + $_[1];
+}
+
+sub div {
+	return $_[0] / $_[1];
+}
+
+
+sub fibo {
+    my ($n) = @_;
+    return $n if $n == 0 or $n == 1;
+
+    my @fib = (0, 1);
+    for (2..$n) {
+        push @fib, $fib[-1] + shift @fib;
+    }
+    return $fib[-1];
+}
+
+1;
+
---
+++ perl/examples/cover/t/test.t
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+use Test::More 'no_plan';
+
+use MyTools qw(add div fibo);
+
+is(add(2, 3), 5);
+
+#is(fibo(0), 0);
+#is(fibo(1), 1);
+#is(fibo(2), 1);
+#is(div(6, 3), 2);
+
+#is(div(6, 0), 0);
+#is(fibo(-1), -1);
