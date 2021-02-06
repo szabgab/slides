3e1998a3fd5c7bb4ba2d26ab71638e12889aebcf
Gabor Szabo <gabor@szabgab.com>
2021-02-06 12:28:35 +0200

    simplify call

--- perl/examples/bdd/calculator/features/step_definitions/some_steps.pl
+++ perl/examples/bdd/calculator/features/step_definitions/some_steps.pl
@@ -12,17 +12,14 @@ use lib File::Spec->catfile(dirname(__FILE__), '..', '..', 'lib');
 use Calculator;
 
 Given 'the calculator app', sub {
-    S->{'calculator'} = 'Calculator';
 };
-#
+
 #When qr/^calling hello_world function/, sub {
 #    S->{'result'}  = HelloWorld::hello_world();
 #};
-#
+
 Then qr/display is "([^"]*)"/, sub {
     my $expected = C->matches->[0];
-    my $function = S->{'calculator'} . '::display';
-    no strict 'refs';
-    is $function->(), $expected;
+    is Calculator::display(), $expected;
 };
 
