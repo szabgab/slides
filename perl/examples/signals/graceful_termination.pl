0b06946814587eb87febb246a62fde5c834ce177
Gabor Szabo <gabor@szabgab.com>
2021-02-12 10:25:30 +0200

    update

---
+++ perl/examples/signals/graceful_termination.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "kill -15 $$    or press Ctrl-C    to stop\n";
+my $continue = 1;
+$SIG{TERM} = sub {
+    print "TERM received\n";
+    $continue = 0;
+};
+
+$SIG{INT} = sub {
+    print "INT received\n";
+    $continue = 0;
+};
+
+while ($continue) {
+    if (open my $fh, '>>', 'process.log') {
+        print $fh scalar localtime();
+        print $fh "\n";
+    }
+    sleep 1;
+}
+
