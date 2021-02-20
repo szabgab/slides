aba42bd1f60e25c4f3e28e98c6f8607cf6c99999
Gabor Szabo <gabor@szabgab.com>
2021-02-20 20:22:29 +0200

    stop immediately

--- docker/examples/deploy-stand-alone-python/deploy.sh
+++ docker/examples/deploy-stand-alone-python/deploy.sh
@@ -1,5 +1,5 @@
 docker build -t flasker .
-docker container stop flask
+docker container stop flask --time 0
 docker container rm flask
 docker run -d --name flask -v/data:/data --env COUNTER_DIR=/data --restart unless-stopped  -p5000:5000 flasker
 
