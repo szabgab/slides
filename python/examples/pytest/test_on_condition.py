527f439a5fc9e1f1938942dedd012e97f5f6963e
Gabor Szabo <gabor@szabgab.com>
2021-02-24 21:38:07 +0200

    remove whitespace

--- python/examples/pytest/test_on_condition.py
+++ python/examples/pytest/test_on_condition.py
@@ -7,11 +7,11 @@ def test_mac():
 
 @pytest.mark.skipif(sys.platform != 'linux', reason="Linux tests")
 def test_linux():
-    assert True 
+    assert True
 
 @pytest.mark.skipif(sys.platform != 'win32', reason="Windows tests")
 def test_windows():
-    assert True 
+    assert True
 
 @pytest.mark.skip(reason="To show we can skip tests without any condition.")
 def test_any():
