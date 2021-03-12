bb217d102b82fe5048acc73f5bbc695b677a5b69
Gabor Szabo <gabor@szabgab.com>
2021-03-12 09:46:30 +0200

    update exercies explanation and solution as well

--- python/examples/files/text_report.py
+++ python/examples/files/text_report.py
@@ -1,34 +1,75 @@
 import sys
 
-if len(sys.argv) !=2:
-    exit(f"Usage: {sys.argv[0]} FILENAME")
-    # text_report.txt
 
-in_file = sys.argv[1]
+def main():
+    if len(sys.argv) !=2:
+        exit(f"Usage: {sys.argv[0]} FILENAME")
+        # text_report.txt
 
-with open(in_file) as fh:
-    for row in fh:
-        row = row.rstrip("\n")
-        if 'Report:' in row:
-            print(row)
+    in_file = sys.argv[1]
+    show_rows_with_report(in_file)
+    show_rows_start_with_report(in_file)
+    show_numbers_after_report(in_file)
+    sum_numbers_after_report(in_file)
+    sum_numbers_after_report_within_begin_end_section(in_file)
 
-print('-' * 20)
 
-with open(in_file) as fh:
-    for row in fh:
-        row = row.rstrip("\n")
-        if row.startswith('Report:'):
-            print(row)
+def show_rows_with_report(in_file):
+    with open(in_file) as fh:
+        for row in fh:
+            row = row.rstrip("\n")
+            if 'Report:' in row:
+                print(row)
+    print('-' * 20)
 
-print('-' * 20)
+def show_rows_start_with_report(in_file):
+    with open(in_file) as fh:
+        for row in fh:
+            row = row.rstrip("\n")
+            if row.startswith('Report:'):
+                print(row)
+    print('-' * 20)
 
-total = 0
-with open(in_file) as fh:
-    for row in fh:
-        row = row.rstrip("\n")
-        #if row.startswith('Report:'):
-        if 'Report:' in row:
-            parts = row.split(':')
-            print(int(parts[1]))
-            total += int(parts[1])
-print(f"Total: {total}")
+def show_numbers_after_report(in_file):
+    with open(in_file) as fh:
+        for row in fh:
+            row = row.rstrip("\n")
+            if 'Report:' in row:
+                parts = row.split(':')
+                print(int(parts[1]))
+    print('-' * 20)
+
+def sum_numbers_after_report(in_file):
+    total = 0
+    with open(in_file) as fh:
+        for row in fh:
+            row = row.rstrip("\n")
+            if 'Report:' in row:
+                parts = row.split(':')
+                total += int(parts[1])
+    print(f"Total: {total}")
+    print('-' * 20)
+
+def sum_numbers_after_report_within_begin_end_section(in_file):
+    in_section = False
+    total = 0
+    with open(in_file) as fh:
+        for row in fh:
+            row = row.rstrip("\n")
+            if row == 'Begin report':
+                in_section = True
+                continue
+            if row == 'End report':
+                in_section = False
+                continue
+            if in_section:
+                if 'Report:' in row:
+                    parts = row.split(':')
+                    total += int(parts[1])
+                    print(int(parts[1]))
+    print(f"Total in section: {total}")
+    print('-' * 20)
+
+
+main()
+
--- python/files.md
+++ python/files.md
@@ -289,19 +289,23 @@ This does NOT work because files can only be accessed sequentially.
 ## Exercise: print lines with Report:
 {id: exercise-print-lines-with-report}
 
+
+In many cases you get some text report in some free form of text (and not in a CSV file or an Excel file.) You need to extract the information
+from such a file after recognizing the patterns. This exercise tries to provide such a case.
+
 * Create a script called **text_report.py**
 
 Given a file that looks like this:
 
 ![](examples/files/text_report.txt)
 
-* Print out the first line that starts with Report:
-* Print out all the lines that have Report: in it
-* Print out all the lines that start with Report:
-* Print out the numbers that are after Report:
-* Add the numbers that after after Report:
+* Print out the first line that starts with `Report:`.
+* Print out all the lines that have the string `Report:` in it.
+* Print out all the lines that start with the string `Report:`.
+* Print out the numbers that are after `Report:`. (e.g. `Report: 42` print out 42)
+* Add the numbers that after after the string `Report:`. So in the above example the result is expected to be 204.
 
-* Now do the same, but only take account lines between the Begin and End secition.
+* Do the same, but only take account lines between the `Begin report` and `End report` section. (sum expected to be 58)
 
 
 ## Exercise: color selector
