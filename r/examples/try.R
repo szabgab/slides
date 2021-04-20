6167bd634198a18477e1041da71785cd3cee19f5
Gabor Szabo <gabor@szabgab.com>
2021-04-20 23:10:20 +0300

    update try

--- r/examples/try.R
+++ r/examples/try.R
@@ -1,12 +1,15 @@
-barplot(c(2, 3, 7, 1))
-data <- rnorm(100)
-barplot(data)
+args
 
-fruits = c("apple", "apple", "banana", "peach", "apple", "banana")
-fruits
-class(fruits)
-# A Factor is also a vector?? but with levels (which are the unique values in the original vector)
-ff = as.factor(fruits)
-class(ff)
-length(ff)
-ff
+# 
+# barplot(c(2, 3, 7, 1))
+# data <- rnorm(100)
+# barplot(data)
+# 
+# fruits = c("apple", "apple", "banana", "peach", "apple", "banana")
+# fruits
+# class(fruits)
+# # A Factor is also a vector?? but with levels (which are the unique values in the original vector)
+# ff = as.factor(fruits)
+# class(ff)
+# length(ff)
+# ff
