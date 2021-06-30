ef795b39debecf3f39493f35a63ed915b3d52cb1
Gabor Szabo <gabor@szabgab.com>
2021-06-30 18:41:28 +0300

    make ameba happy

--- crystal/examples/regexes/gsub.cr
+++ crystal/examples/regexes/gsub.cr
@@ -1,5 +1,10 @@
 puts "The cat is here".gsub("cat", "dog")
 puts "The cat is here".gsub("cat") { "dog" }
 
-puts "The cat is here".gsub("cat") { |match| match.reverse }
-puts "The cat is here".gsub(/c../) { |match| match.reverse }
+# puts "The cat is here".gsub("cat") { |match| match.reverse }
+# ameba: Use short block notation instead
+puts "The cat is here".gsub("cat", &.reverse)
+
+# puts "The cat is here".gsub(/c../) { |match| match.reverse }
+# ameba: Use short block notation instead
+puts "The cat is here".gsub(/c../, &.reverse)
