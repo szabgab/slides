65462687614f064ecfe39a66c3aaafc6cb9fa920
Gabor Szabo <gabor@szabgab.com>
2021-09-02 11:59:28 +0300

    change name

--- python/examples/pytest/fixture-with-teardown/test_app.py
+++ python/examples/pytest/fixture-with-teardown/test_app.py
@@ -3,20 +3,20 @@ import application
 
 
 @pytest.fixture()
-def getapp():
-    print('getapp starts')
+def myapp():
+    print('myapp starts')
     app = application.App()
 
     yield app
 
     app.shutdown()
-    print('getapp ends')
+    print('myapp ends')
 
-def test_add_user_foo(getapp):
-    getapp.add_user("Foo")
-    assert getapp.get_user() == 'Foo'
+def test_add_user_foo(myapp):
+    myapp.add_user("Foo")
+    assert myapp.get_user() == 'Foo'
 
-def test_add_user_bar(getapp):
-    getapp.add_user("Bar")
-    assert getapp.get_user() == 'Bar'
+def test_add_user_bar(myapp):
+    myapp.add_user("Bar")
+    assert myapp.get_user() == 'Bar'
 
