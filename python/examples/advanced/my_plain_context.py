46761c0a322120e0c9fa861529e4db647dbc7893
Gabor Szabo <gabor@szabgab.com>
2021-09-14 11:29:32 +0300

    update

--- python/examples/advanced/my_plain_context.py
+++ python/examples/advanced/my_plain_context.py
@@ -1,14 +1,30 @@
 from contextlib import contextmanager
+import sys
+
+param = ''
+if len(sys.argv) == 2:
+    #exit(f"Usage: {sys.argv[0]} []")
+    param = sys.argv[1]
+
+def code_with_context_manager():
+    with my_plain_context():
+        print("  In plain context")
+        if param == "return":
+            return
+        if param == "die":
+            raise Exception("we have a problem")
+        print("  More work")
+
 
 @contextmanager
 def my_plain_context():
-    print("start context")
-    yield
-    print("end context")
+    print("setup context")
+    try:
+        yield
+    except Exception as err:
+        print(f"  We got an exception: {err}")
+    print("cleanup context")
 
 print("START")
-with my_plain_context():
-    print("  In plain context")
-    print("  More work")
-
+code_with_context_manager()
 print("END")
