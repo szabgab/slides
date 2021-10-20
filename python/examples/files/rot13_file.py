7bd2718f8d73e678d1d2f3cc99fbe6a57b2438d0
Gabor Szabo <gabor@szabgab.com>
2021-10-20 13:23:40 +0300

    remove unnecessary extra function

--- python/examples/files/rot13_file.py
+++ python/examples/files/rot13_file.py
@@ -1,14 +1,6 @@
 import sys
 import codecs
 
-
-def rot13(text):
-    text_in_rot_13 = ''
-    # Do something with text to become  rot13
-
-    return text_in_rot_13
-
-
 if len(sys.argv) != 2:
     exit(f"Usage: {sys.argv[0]} FILENAME")
 
