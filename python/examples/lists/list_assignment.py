471a019ceb0687b1ce885c437e72f16649700897
Gabor Szabo <gabor@szabgab.com>
2022-03-10 15:00:15 +0100

    example

--- python/examples/lists/list_assignment.py
+++ python/examples/lists/list_assignment.py
@@ -6,3 +6,8 @@ x, y = y, x
 print(x)      # 2
 print(y)      # 1
 
+def stats(num):
+    return sum(num), sum(num)/len(num), min(num), max(num)
+
+total, average, minimum, maximum = stats([2, 3, 4])
+print(total, average, minimum, maximum) # 9 3.0 2 4
