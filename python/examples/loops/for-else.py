8c0d97b24a21ae61c91778e9f6ef0336625ad08f
Gabor Szabo <gabor@szabgab.com>
2022-03-21 10:51:44 +0100

    update

--- python/examples/loops/for-else.py
+++ python/examples/loops/for-else.py
@@ -1,3 +1,32 @@
+found_number_bigger_than_10 = False
+
+numbers = [2, 3, 4]
+for num in numbers:
+    if num > 10:
+        found_number_bigger_than_10 = True
+        break
+    print(num)
+
+if found_number_bigger_than_10:
+    print("found number bigger than 10")
+
+print('---------------------')
+
+found_number_bigger_than_10 = False
+
+numbers = [2, 3, 12, 4]
+for num in numbers:
+    if num > 10:
+        found_number_bigger_than_10 = True
+        break
+    print(num)
+
+if found_number_bigger_than_10:
+    print("found number bigger than 10")
+
+print('---------------------')
+
+
 
 for num in [2, 3, 4]:
     if num > 10:
@@ -5,11 +34,15 @@ for num in [2, 3, 4]:
     print(num)
 else:
     print("in else - finished without calling break")
-print('')
+    print("not found number bigger than 10")
 
-for num in [2, 3, 12]:
+print('---------------------')
+
+for num in [2, 3, 12, 4]:
     if num > 10:
         break
     print(num)
 else:
     print("in else - finished after calling break")
+    print("not found number bigger than 10")
+
