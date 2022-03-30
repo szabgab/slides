dbecb61e1c84d33b98de2fe7f44409551f8daba1
Gabor Szabo <gabor@szabgab.com>
2022-03-30 10:21:30 +0300

    preventing?

---
+++ python/examples/exceptions/preventing_divide_by_zero.py
@@ -0,0 +1,13 @@
+def div(a, b):
+    if b == 0:
+        # raise Exception("Cannot divide by zero")
+        print("Cannot divide by zero")
+        return None
+
+    print("before")
+    print(a/b)
+    print("after")
+
+div(1, 0)
+
+
