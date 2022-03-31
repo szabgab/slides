ce63872b6e64e27404fb60d7b98584c7e85df16d
Gabor Szabo <gabor@szabgab.com>
2022-03-31 11:12:23 +0300

    update

--- python/examples/exceptions/demo1.py
+++ python/examples/exceptions/demo1.py
@@ -1,8 +1,11 @@
-main()
-    ...
+def some_function()
     result = do_something(filename)
     if result:
         do_something_else(result)
     else:
         return result
+
+main()
+    ...
+    result = some_function()
 
