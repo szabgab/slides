ce33b96960a96abd136e46d2e3959dc36e82e904
Gabor Szabo <gabor@szabgab.com>
2022-04-06 13:51:07 +0300

    update

--- python/examples/boolean/compare_two_strings.py
+++ python/examples/boolean/compare_two_strings.py
@@ -6,3 +6,21 @@ if x < y:
 
 # x is less than y
 
+x = "מלון"
+y = "בלון"
+
+if x < y:
+    print(f"{x} in x is less than {y}")
+else:
+    print(f"{y} in y is less than {x}")
+
+
+x = "👸"
+y = "💂"
+
+if x < y:
+    print(f"{x} in x is less than {y}")
+else:
+    print(f"{y} in y is less than {x}")
+
+
