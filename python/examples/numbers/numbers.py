74a01459cb8decdf5464be2955b06e6ae5d97a62
Gabor Szabo <gabor@szabgab.com>
2022-04-06 11:53:04 +0300

    update

--- python/examples/numbers/numbers.py
+++ python/examples/numbers/numbers.py
@@ -1,15 +1,15 @@
-a = 42   # decimal
-h = 0xA  # 10 - hex            - staring with 0x
-o = 0o11 # 9  - octal          - starting with 0o
-         # 011 works in Python 2.x but Python 3.x
-         # requires the o that works in
-         # (recent versions of) Python 2.x
-b = 0b11 # 3  - binary numbers - starting with 0b
+a = 42     # decimal
+h = 0xA3C  # 2620 - hex           - staring with 0x
+o = 0o171  # 121  - octal         - starting with 0o
+           # 011 works in Python 2.x but not in Python 3.x
+           # requires the o that works in
+           # (recent versions of) Python 2.x
+b = 0b101  # 5  - binary numbers - starting with 0b
 
 r = 2.3
 
-print(a)  # 42
-print(h)  # 10
-print(o)  #  9
-print(b)  #  3
+print(a)  #   42
+print(h)  # 2620
+print(o)  #  121
+print(b)  #    5
 print(r)  #  2.3
