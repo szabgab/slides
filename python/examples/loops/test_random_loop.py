fabc77b710b300f99bf15c6cc97114dc7bb73b29
Gabor Szabo <gabor@szabgab.com>
2022-04-07 10:39:37 +0300

    parametrize test so the final print will work

--- python/examples/loops/test_random_loop.py
+++ python/examples/loops/test_random_loop.py
@@ -2,24 +2,24 @@ import while_break
 import while_complex_condition
 import while_true
 
-import sys
 import random
+import pytest
 
-def test_random_loop(capsys):
-    for seed in [0, 7, 9, 21]:
-        random.seed(seed)
-        while_complex_condition.random_loop()
-        out_complex, _ = capsys.readouterr()
+@pytest.mark.parametrize('seed', [0, 7, 9, 21])
+def test_random_loop(capsys, seed):
+    random.seed(seed)
+    while_complex_condition.random_loop()
+    out_complex, _ = capsys.readouterr()
 
-        random.seed(seed)
-        while_break.random_loop()
-        out_break, _ = capsys.readouterr()
+    random.seed(seed)
+    while_break.random_loop()
+    out_break, _ = capsys.readouterr()
 
-        assert out_complex == out_break
+    assert out_complex == out_break
 
-        random.seed(seed)
-        while_true.random_loop()
-        out_true, _ = capsys.readouterr()
-        assert out_complex == out_true
+    random.seed(seed)
+    while_true.random_loop()
+    out_true, _ = capsys.readouterr()
+    assert out_complex == out_true
 
-        print(out_true, file=sys.stderr)
+    print(out_true)
