520ce27af56bb448e60597c0598a0ba49417822d
Gabor Szabo <gabor@szabgab.com>
2022-04-11 08:27:13 +0300

    rename

--- python/examples/dictionary/exists.py
+++ python/examples/dictionary/exists.py
@@ -1,15 +1,17 @@
 user = {
     'fname': 'Foo',
     'lname': 'Bar',
+    'answer': None,
 }
 
 print('fname' in user)  # True
 print('email' in user)  # False
+print('answer' in user) # True
 print('Foo' in user)    # False
 
-for k in ['fname', 'email', 'lname']:
-    if k in user:
-        print("{} => {}".format(k, user[k]))
+for attr in ['fname', 'email', 'lname']:
+    if attr in user:
+        print("{} => {}".format(attr, user[attr]))
 
 # fname => Foo
 # lname => Bar
