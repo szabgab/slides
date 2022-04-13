65b32199dd50ae14981183bfbc35830bec0abae0
Gabor Szabo <gabor@szabgab.com>
2022-04-13 08:19:35 +0300

    roundig

--- python/examples/datetime/datetime_rounding.py
+++ python/examples/datetime/datetime_rounding.py
@@ -5,3 +5,5 @@ rounded = now - datetime.timedelta(microseconds=now.microsecond)
 print(now)     # 2019-11-01 07:11:19.930974
 print(rounded) # 2019-11-01 07:11:19
 
+ts = datetime.datetime.now().replace(microsecond=0)
+
