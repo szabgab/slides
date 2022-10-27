bc7dfd26f80dcd8a19669d6a24af6f50427604c3
Gabor Szabo <gabor@szabgab.com>
2022-10-27 09:52:46 +0300

    report unused files

--- rust/check.rs
+++ rust/check.rs
@@ -1,6 +1,11 @@
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::exit;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::iter::FromIterator;
+
+// use regex::Regex;
 
 // from all the md files extract the list of included files
 // from the examples/ directory list all the files
@@ -11,12 +16,22 @@ use std::process::exit;
 fn main() {
     //get_md_files()
     let md_files = get_md_files();
-    println!("{:?}", md_files);
+    // println!("{:?}", md_files);
+    let imported_files = get_imported_files(md_files);
+    // println!("{:?}", imported_files);
     let examples = get_examples();
-    println!("{:?}", examples);
+    //println!("{:?}", examples);
+    let mut count = 0;
+    for filename in examples {
+        if ! imported_files.contains(&filename) {
+            println!("Unused file: `{}`", filename);
+            count += 1;
+        }
+    }
+    println!("There are {count} unused examples");
 }
 
-fn get_examples() -> Vec<PathBuf> {
+fn get_examples() -> Vec<String> {
     let mut examples = vec![];
     let path = Path::new("examples");
     for entry in path.read_dir().expect("read_dir call failed") {
@@ -26,7 +41,7 @@ fn get_examples() -> Vec<PathBuf> {
                 //println!("dir");
                 for filepath in dirname.read_dir().expect("read_dir call failed") {
                     if let Ok(filepath) = filepath {
-                        // println!("{:?}", filepath);
+                        //println!("{:?}", filepath);
                         let filename = filepath.path();
                         examples.push(filename);
                     }
@@ -37,7 +52,33 @@ fn get_examples() -> Vec<PathBuf> {
             }
         }
     }
-    return examples
+    return Vec::from_iter( examples.iter().map(|s| s.clone().into_os_string().into_string().expect("Bad") ) );
+}
+
+fn get_imported_files(md_files: Vec<PathBuf>) -> Vec<String> {
+    // println!("{:?}", md_files);
+    // ![](examples/arrays/update_hash.rs)
+    // let re = Regex::new(r"^!\[\]]\((.*)\)\s*$").unwrap();
+    let mut imported_files = vec![];
+    for filename in md_files {
+        //println!("{:?}", filename);
+        match File::open(filename.as_path()) {
+            Ok(file) => {
+                let reader = BufReader::new(file);
+                for line in reader.lines() {
+                    let line = line.unwrap();
+                    if line.starts_with("![](") && line.ends_with(")") {
+                        //println!("{}", &line[4..line.len()-1])
+                        imported_files.push((&line[4..line.len()-1]).to_string());
+                    }
+                }
+            },
+            Err(error) => {
+                println!("Error opening file {:?}: {}", filename, error);
+            },
+        }
+    }
+    return Vec::from_iter( imported_files.iter().map(|s| s.to_string()) );
 }
 
 fn get_md_files() -> Vec<PathBuf> {
