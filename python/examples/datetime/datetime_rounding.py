bfdf7e7aaf36a53a197a8562cc8be3b90c9a93db
Gabor Szabo <gabor@szabgab.com>
2022-11-15 07:39:43 +0200

    clarify

--- python/examples/datetime/datetime_rounding.py
+++ python/examples/datetime/datetime_rounding.py
@@ -1,9 +1,11 @@
 import datetime
 
+# Old solution
 now = datetime.datetime.now()
 rounded = now - datetime.timedelta(microseconds=now.microsecond)
 print(now)     # 2019-11-01 07:11:19.930974
 print(rounded) # 2019-11-01 07:11:19
 
+# A simpler solution
 ts = datetime.datetime.now().replace(microsecond=0)
-
+print(ts)
