594f17e213f2ab298021f17c3364f74b2efb2d60
Gabor Szabo <gabor@szabgab.com>
2022-12-07 12:25:47 +0200

    only open localhost

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def any_page(relpath):
     return show(relpath)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
+    #app.run(host="0.0.0.0", port=5000)
