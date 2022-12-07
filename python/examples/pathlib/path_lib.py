9256691eac0762cf55853cdec65da49c655d9b71
Gabor Szabo <gabor@szabgab.com>
2022-12-07 16:07:44 +0200

    add path

---
+++ python/examples/pathlib/path_lib.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+this = Path(__file__)
+print(this)
+print(this.parent)
+print(this.parents[1])
+
+print(list(this.parent.iterdir()))
+# .exists
+# joinpath or /
---
+++ python/path.md
@@ -0,0 +1,9 @@
+# Path
+{id: path}
+
+## Pathlib example
+{id: pathlib-example}
+
+The slide needs to be Improved!
+
+![](examples/pathlib/path_lib.py)
