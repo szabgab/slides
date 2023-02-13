60bd890f3a3f8fa6bb88fd03b2ea4f11804cc641
Gabor Szabo <gabor@szabgab.com>
2023-02-13 17:35:30 +0200

    don't special case the CI

--- generate_slides.py
+++ generate_slides.py
@@ -13,8 +13,6 @@ import datetime
 root   = os.path.dirname(os.path.abspath(__file__))
 parent = os.path.dirname(root)
 slider = "{}/slider-py/slider.py".format(parent)
-if os.environ.get('TRAVIS') or os.environ.get('GITHUB_WORKFLOW'):
-    slider = "{}/slider-py/slider.py".format(root)
 #print("root={}".format(root))
 #print("parent={}".format(parent))
 if not os.path.exists(slider):
