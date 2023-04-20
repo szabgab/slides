3dbd625c7e04f351e8197048566d3701c1d1a681
Gabor Szabo <gabor@szabgab.com>
2023-04-20 07:29:41 +0300

    update example

--- python/examples/pil/resize_image_demo.py
+++ python/examples/pil/resize_image_demo.py
@@ -1,24 +1,24 @@
 from PIL import Image
 import sys
 
-if len(sys.argv) != 3:
-    exit(f"Usage: {sys.argv[0]} FILENAME CHANGE%")
+if len(sys.argv) != 3 and len(sys.argv) != 4:
+    exit(f"Usage: {sys.argv[0]} FILENAME %CHANGE OUTFILE")
 
 in_file = sys.argv[1]
 change = float(sys.argv[2])
+out_file = sys.argv[3] if len(sys.argv) == 4 else None
 
 img = Image.open(in_file)
 print(img.size)    # a tuple
 print(img.size[0]) # width
 print(img.size[1]) # height
 
-print(sys.getsizeof(img)) # The size of the variable
-print(sys.getsizeof(img.tobytes())) # The payload
-
 width = int(change * img.size[0] / 100)
 height = int(change * img.size[1] / 100)
 
 out = img.resize((width, height))
 out.show()
-#out.save(args.out)
+
+if out_file:
+    out.save(out_file)
 
