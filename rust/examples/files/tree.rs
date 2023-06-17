71ca2de6e631498ab892ededdee6d20f1a32503d
Gabor Szabo <gabor@szabgab.com>
2023-06-17 10:23:37 +0300

    improve

--- rust/examples/files/tree.rs
+++ rust/examples/files/tree.rs
@@ -11,25 +11,24 @@ fn main() {
         exit(1);
     }
 
-    let mut path_str = ".";
+    let mut path = ".";
     if args.len() == 2 {
         println!("{}", args[1]);
-        path_str = &args[1];
+        path = &args[1];
     }
 
-    list_dir(path_str);
+    list_dir(Path::new(path));
 }
 
 
-fn list_dir(path_str: &str) {
-    let path = Path::new(path_str);
+fn list_dir(path: &Path) {
     for entry in path.read_dir().expect("read_dir call failed") {
         if let Ok(entry) = entry {
             println!("{:?}", entry.path());
             let metadata = fs::metadata(entry.path());
             let file_type = metadata.expect("Could not access file").file_type();
             if file_type.is_dir() {
-                list_dir(entry.path());
+                list_dir(&entry.path());
             }
         }
     }
