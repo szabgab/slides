490f2a65b734f0e93c16011a02ea1c5e69d9c440
Gabor Szabo <gabor@szabgab.com>
2023-06-17 07:38:27 +0300

    add code

--- rust/examples/number_guessing_game/src/main.rs
+++ rust/examples/number_guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Number Guessing game");
+    println!("Print input your guess");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed {guess}");
 }
