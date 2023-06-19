6bd00936465ecccd3dd76a958e5a8c6551c46183
Gabor Szabo <gabor@szabgab.com>
2023-06-19 11:22:09 +0300

    add date examples

--- rust/examples/handle-time/src/main.rs
+++ rust/examples/handle-time/src/main.rs
@@ -1,5 +1,6 @@
 //use time::PrimitiveDateTime as DateTime;
 use time::Date;
+use time::Month;
 
 //    DateTime::new(
 //        Date::from_calendar_date(year, month.try_into().unwrap(), day).unwrap(),
@@ -8,10 +9,52 @@ use time::Date;
 
 
 fn main() {
-    println!("Hello, world!");
+    let date = Date::from_calendar_date(2023, Month::June, 18);
+    println!("{:?}", date);
+
+    one();
+    two();
+    three();
+
+    other();
+}
+
+fn one() {
     let year = 2023;
     let month = 6;
     let day = 18;
     let date = Date::from_calendar_date(year, month.try_into().unwrap(), day).unwrap();
-    println!("{:?}", date);
+    print_date(date);
 }
+
+fn two() {
+    let year = 2023;
+    let month = Month::June;
+    let day = 18;
+    let date = Date::from_calendar_date(year, month, day).unwrap();
+    print_date(date);
+}
+
+fn three() {
+    let year = 2023;
+    let month = 6;
+    let month: Month = month.try_into().unwrap();
+    let day = 18;
+    let date = Date::from_calendar_date(year, month, day).unwrap();
+    print_date(date);
+}
+
+fn other() {
+    let date = Date::from_calendar_date(2023, Month::June, 18).unwrap();
+    println!("{:?}", date.midnight());
+}
+
+fn print_date(date: Date) {
+    println!("{:?}", date);
+    println!("{}", date.year());
+    println!("{}", date.month());
+    println!("{}", date.day());
+    println!("");
+}
+
+
