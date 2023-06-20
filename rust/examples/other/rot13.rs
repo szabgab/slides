0b0c0e76e7a3b7b8fd32e2ecbac6a3a068dd1a5b
Gabor Szabo <gabor@szabgab.com>
2023-06-20 11:39:29 +0300

    fix the rot13

--- rust/examples/other/rot13.rs
+++ rust/examples/other/rot13.rs
@@ -1,35 +1,37 @@
 fn main() {
-    let text = "Hello World!";
+    let text = String::from("Hello World!");
     println!("{text}");
     let encrypted = rot13(text);
     println!("{encrypted}");
-    //let decrypted = rot13(encrypted);
-    //println!("{decrypted}");
+    let decrypted = rot13(encrypted);
+    println!("{decrypted}");
 
     //let equal = text == decrypted;
     //println!("{equal}");
 }
 
-fn rot13(text: &str) -> &str {
+fn rot13(text: String) -> String {
     let mut chars:Vec<char> = vec![];
-    let mut strs:Vec<String> = vec![];
     let a = 'a' as u32;
     let z = 'z' as u32;
+    let aa = 'A' as u32;
+    let zz = 'Z' as u32;
     for ch in text.chars() {
-        // dbg!(ch);
         let mut code = ch as u32;
-        dbg!(code);
+        // dbg!(code);
         if a <= code && code <= z {
             code = a + ((code - a + 13) % 26);
-            //dbg!(code);
-            //
             let ch_new = char::from_u32(code).expect("Could not convert to char");
             chars.push(ch_new);
-            strs.push(ch_new.to_string());
-            //code = code + 13
+        } else if aa <= code && code <= zz {
+            code = aa + ((code - aa + 13) % 26);
+            let ch_new = char::from_u32(code).expect("Could not convert to char");
+            chars.push(ch_new);
+        } else {
+            chars.push(ch);
         }
     }
-    //chars.iter().collect()
-    strs.iter().collect()
-    //text
+
+    let other: String = chars.into_iter().collect();
+    other
 }
