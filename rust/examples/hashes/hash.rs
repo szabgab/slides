3b32fbd9ab69c640f61aa8dab9662b4ba18223e3
Gabor Szabo <gabor@szabgab.com>
2023-06-29 15:07:23 +0300

    examples

--- rust/examples/hashes/hash.rs
+++ rust/examples/hashes/hash.rs
@@ -2,6 +2,19 @@ use std::collections::HashMap;
 
 fn main() {
     let mut counter = HashMap::new();
+    println!("{}", counter.len());
+
     counter.insert("foo", 1);
     counter.insert("bar", 2);
+    println!("{:?}", counter);
+    println!("{:?}", counter.len());
+    println!("{:?}", counter.keys());
+
+    for name in counter.keys() {
+        println!("{} : {}", name, counter[name]);
+    }
+
+    for (name, value) in counter {
+        println!("{} : {}", name, value);
+    }
 }
