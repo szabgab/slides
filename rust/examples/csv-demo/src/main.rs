ea04ee98730f3dc0820fc3ba81efb5dc6c8f2d64
Gabor Szabo <gabor@szabgab.com>
2023-07-25 09:58:32 +0300

    update

--- rust/examples/csv-demo/src/main.rs
+++ rust/examples/csv-demo/src/main.rs
@@ -1,35 +1,35 @@
-//use std::{error::Error, io, process};
 use std::fs::File;
 
 
 fn main() {
     let filepath = "planets.csv";
-    //println!("{}", filepath);
     let rows = read_file(filepath);
-    for row in rows {
+    for row in &rows {
         println!("{:?}", row);
+        println!("{}", &row[0]);
     }
+    println!("------");
+    for value in &rows[0] {
+        println!("{}", value);
+    }
+    println!("------");
+
+    println!("{}", &rows[3][0]);
+
 }
 
 fn read_file(filepath: &str) -> Vec<csv::StringRecord> {
     let mut rows:Vec<csv::StringRecord> = vec![];
     match File::open(filepath.to_string()) {
         Ok(file) => {
-            //let mut content = String::new();
-            //file.read_to_string(&mut content).unwrap();
             let mut rdr = csv::Reader::from_reader(file);
             for result in rdr.records() {
                 match result {
                     Ok(row) => {
                         rows.push(row.clone());
-                        //println!("{:?}", row);
-                        //println!("{}", row.len());
-                        //println!("{}", &row[0]);
                     },
                     Err(err) => panic!("Error {}", err)
                 };
-                //let record = result?;
-                //println!("{:?}", record);
             }
         },
         Err(error) => panic!("Error opening file {}: {}", filepath, error),
