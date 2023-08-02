95a452879d4d1d81f482bfdd514662afcc3c2c95
Gabor Szabo <gabor@szabgab.com>
2023-08-02 18:57:26 +0300

    http example

---
+++ rust/examples/http-client/src/main.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+// apt-get install pkg-config
+// apt-get install libssl-dev
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
---
+++ rust/http.md
@@ -0,0 +1,13 @@
+# http
+{id: http}
+
+## http-client with reqwest
+{id: http-client}
+
+* [reqwest](https://crates.io/crates/reqwest)
+* [tokio](https://crates.io/crates/tokio)
+
+![](examples/http-client/Cargo.toml)
+![](examples/http-client/src/main.rs)
+
+
