df36de731db12bad9f1768735b9694cfcca5aaf0
Gabor Szabo <gabor@szabgab.com>
2023-08-11 09:50:10 +0200

    commafy

--- rust/examples/commafy/src/lib.rs
+++ rust/examples/commafy/src/lib.rs
@@ -1,6 +1,11 @@
 pub fn commafy(num: i32) -> String {
     let num = format!("{num}");
-    num
+    let mut ix = 0;
+    let num = num.chars().rev().map(|chr| {
+        ix += 1;
+        if ix % 3 == 1 && ix > 1 { format!(",{chr}") } else { format!("{chr}") }
+    }).collect::<String>();
+    num.chars().rev().collect::<String>()
 }
 
 #[cfg(test)]
@@ -12,6 +17,10 @@ mod tests {
         assert_eq!("1", commafy(1));
         assert_eq!("12", commafy(12));
         assert_eq!("123", commafy(123));
+        assert_eq!("1,234", commafy(1234));
+        assert_eq!("12,345", commafy(12345));
+        assert_eq!("123,456", commafy(123456));
+        assert_eq!("1,234,567", commafy(1234567));
     }
 }
 
