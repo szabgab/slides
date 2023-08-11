cefebcbec8f8b0521df4af75fdb2b52f7164d48b
Gabor Szabo <gabor@szabgab.com>
2023-08-11 15:40:49 +0200

    commafy

--- rust/examples/commafy/src/lib.rs
+++ rust/examples/commafy/src/lib.rs
@@ -1,4 +1,5 @@
-pub fn commafy(num: i32) -> String {
+pub fn commafy<Integer: Into<i64> +  Copy + std::fmt::Debug + std::fmt::Display>(num: Integer) -> String {
+//pub fn commafy(num: i32) -> String {
     let num = format!("{num}");
     let mut ix = 0;
     let num = num.chars().rev().map(|chr| {
@@ -21,6 +22,10 @@ mod tests {
         assert_eq!("12,345", commafy(12345));
         assert_eq!("123,456", commafy(123456));
         assert_eq!("1,234,567", commafy(1234567));
+
+        assert_eq!("1,234", commafy(1234i16));
+        assert_eq!("254", commafy(254u8));
+        assert_eq!("1,254", commafy(1254u16));
     }
 }
 
