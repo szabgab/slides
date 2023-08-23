845f4f95a5e3ab4babb46c3e8725fcf459913f3c
Gabor Szabo <gabor@szabgab.com>
2023-08-23 18:34:25 +0200

    more clap examples

--- rust/examples/clap-example/src/main.rs
+++ rust/examples/clap-example/src/main.rs
@@ -12,16 +12,24 @@ struct Cli {
     #[arg(long, default_value = "")]
     indexfile: String,
 
-    #[arg(long)]
-    nice: bool,
-
     #[arg(long, default_value = ".")]
     path: std::path::PathBuf,
-}
 
+    #[arg(long, default_value_t = 0, help = "A number mapped to i32")]
+    limit: i32,
+
+    #[arg(long, default_value_t = 0, help = "A number mapped to u8")]
+    small: u8,
+
+    #[arg(long, short, default_value_t = false, help = "Turn on debug mode")]
+    debug: bool,
+}
 
 fn main() {
     let args = Cli::parse();
     dbg!(&args.host);
     dbg!(&args);
+    if args.debug {
+        println!("In debug mode");
+    }
 }
