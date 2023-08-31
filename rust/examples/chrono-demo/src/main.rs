50aaf9c5a1777d81bbcfa1c1b546fc0ec7e5d81f
Gabor Szabo <gabor@szabgab.com>
2023-08-31 11:14:13 +0300

    update

--- rust/examples/chrono-demo/src/main.rs
+++ rust/examples/chrono-demo/src/main.rs
@@ -2,10 +2,20 @@ use chrono::{DateTime, Utc, Local};
 
 fn main() {
     let utc: DateTime<Utc> = Utc::now();
-    println!("{}", utc);
-    println!("{}", utc.timestamp());
+    println!("utc:           {}", utc);
+    println!("utc timestamp: {}", utc.timestamp());
+    println!("utc format:    {}", utc.format("%Y-%m-%d"));
 
+    println!("");
     let local: DateTime<Local> = Local::now();
     println!("{}", local);
     println!("{}", local.timestamp());
+
+
+    let text = "2023-08-30T12:30:01+0000".to_string();
+    let dt = DateTime::parse_from_str(&text, "%Y-%m-%dT%H:%M:%S%z").unwrap();
+    println!("");
+    println!("dt:           {}", dt);
+    println!("dt format:    {}", dt.format("%Y-%m-%d"));
+    println!("dt format:    {}", dt.format("%H::%M::%S"));
 }
