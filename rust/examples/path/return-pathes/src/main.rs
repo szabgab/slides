13b29d074fae6fca5b9319dffb4dec19ee282023
Gabor Szabo <gabor@szabgab.com>
2023-10-08 12:07:55 +0300

    add path example

---
+++ rust/examples/path/return-pathes/src/main.rs
@@ -0,0 +1,23 @@
+use std::path::{Path, PathBuf};
+
+fn main() {
+    let pathes = get_pathes();
+    println!("{:?}", pathes);
+    for path in pathes {
+        println!("{:?}", path);
+        //println!("{}", path.display());
+        //println!("{:?}", path.parent());
+    }
+}
+
+fn get_pathes() -> Vec<PathBuf> {
+    let mut entries = vec![];
+    let path = Path::new(".");
+    for entry in path.read_dir().expect("read_dir call failed") {
+        if let Ok(entry) = entry {
+            entries.push(entry.path());
+        }
+    }
+    entries
+}
+
---
+++ rust/path.md
@@ -0,0 +1,13 @@
+# Path
+{id: path}
+
+## Return vector of Path or PathBuf
+{id: return-vector-of-path}
+{i: Path}
+{i: PathBuf}
+
+* We would like to create a function that collectes Path objects and returns them.
+* Instead of Path objects we'll return PathBuf objects
+
+![](examples/path/return-pathes/src/main.rs)
+
