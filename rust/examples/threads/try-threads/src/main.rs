be1479f90b8c740128e1442d94f26eef178f6f35
Gabor Szabo <gabor@szabgab.com>
2023-10-31 10:54:38 +0200

    update

--- rust/examples/threads/try-threads/src/main.rs
+++ rust/examples/threads/try-threads/src/main.rs
@@ -2,21 +2,24 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    println!("Before starting: {:?}", thread::current().id());
+
     let handle = thread::spawn(|| {
         for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
+            println!("hi number {} from the spawned thread! {:?}", i, thread::current().id());
             thread::sleep(Duration::from_millis(1));
         }
         println!("spwaned thread ended");
     });
 
     for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
+        println!("hi number {} from the main thread! {:?}", i, thread::current().id());
         thread::sleep(Duration::from_millis(1));
     }
 
     println!("main thread ended");
-    handle.join().unwrap();
+    handle.join().unwrap(); // waiting for the other thread to end.
+    println!("After ending: {:?}", thread::current().id());
 
     println!("exiting");
 }
--- rust/threads.md
+++ rust/threads.md
@@ -1,10 +1,22 @@
 # Threads in Rust
 {id: threads}
 
+## Threads in Rust
+{id: threads-in-rust}
+
+* [Feerless concurrency](https://doc.rust-lang.org/book/ch16-00-concurrency.html)
+* [std::thread](https://doc.rust-lang.org/std/thread/)
+
 ## First example with threads
 {id: threads-first-example}
+{i: thread}
+{i: spawn}
+{i: sleep}
+{i: join}
+{i: current}
 
 ![](examples/threads/try-threads/src/main.rs)
+![](examples/threads/try-threads/out.out)
 
 ## Threads with messages
 {id: threads-with-messages}
