4693f668f717a3b0f99cb557b50f0b4ff9f27f42
Gabor Szabo <gabor@szabgab.com>
2024-06-20 16:30:00 +0300

    avoid borders

--- python/examples/pil/draw_triangle.py
+++ python/examples/pil/draw_triangle.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw
 img = Image.new(mode='RGB', size=(800, 450), color='#eb8634')
 
 draw = ImageDraw.Draw(img)
-draw.polygon([(800, 275), (800, 450), (300, 450) ])
+draw.polygon([(790, 275), (790, 430), (300, 430) ])
 
 img.save('first.png')
 img.show()
