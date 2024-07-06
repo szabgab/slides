3bfe5001c9bf660805d9858b388a23b824a8293f
Gabor Szabo <gabor@szabgab.com>
2024-07-06 11:04:09 +0300

    update slides

--- python/examples/functional/create_dict_using_zip.py
+++ python/examples/functional/create_dict_using_zip.py
@@ -2,5 +2,9 @@ names = ['Jan', 'Feb', 'Mar', 'Apr']
 days =  [31, 28, 31, 30]
 
 zipped = zip(names, days)
+print(zipped)
+print(list(zipped))
+
+zipped = zip(names, days)
 month = dict(zipped)
 print(month)
--- python/examples/functional/map.py
+++ python/examples/functional/map.py
@@ -5,5 +5,8 @@ numbers = [1, 2, 3, 4]
 
 double_numbers = map(double, numbers)
 print(double_numbers)   # <map object at 0x7f8eb2d849e8>
-for num in double_numbers:
-    print(num)
+#for num in double_numbers:
+#    print(num)
+
+dbl = list(double_numbers)
+print(dbl)
