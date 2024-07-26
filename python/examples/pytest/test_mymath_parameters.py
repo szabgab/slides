246936f4c57fce650a7789963f475d3d576716ec
Gabor Szabo <gabor@szabgab.com>
2024-07-26 07:33:16 +0300

    improve example

--- python/examples/pytest/test_mymath_parameters.py
+++ python/examples/pytest/test_mymath_parameters.py
@@ -1,6 +1,16 @@
 import mymath
 import pytest
 
+def test_add_all():
+    assert mymath.add(2, 3)  == 5
+    assert mymath.add(-1, 1)  == 0
+
+def test_add_1():
+    assert mymath.add(2, 3)  == 5
+
+def test_add_2():
+    assert mymath.add(-1, 1)  == 0
+
 @pytest.mark.parametrize("a,b,result", [
     (2, 3, 5),
     (-1, 1, 0),
