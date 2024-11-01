9facc6dc989b420f654cf8d5e2d8762a65f1b8de
Gabor Szabo <gabor@szabgab.com>
2024-11-01 12:31:51 +0200

    fastapi

--- python/examples/fastapi/hello-world/test_main.py
+++ python/examples/fastapi/hello-world/test_main.py
@@ -8,5 +8,6 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
     assert response.json() == {"message": "Hello World"}
 
