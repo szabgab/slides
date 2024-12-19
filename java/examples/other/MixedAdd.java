3d6883e7294b5c3c0d16f32e6922da1a25f1df29
Gabor Szabo <gabor@szabgab.com>
2024-12-19 08:25:39 +0200

    add

--- java/examples/other/MixedAdd.java
+++ java/examples/other/MixedAdd.java
@@ -3,7 +3,11 @@ public class MixedAdd {
         String a = "19";
         String b = "23";
         int c = 23;
+        double d = 2.3;
+        float f = 2.3f;
         System.out.println("The sum: " + (a+b));
         System.out.println("The sum: " + (a+c));
+        System.out.println("The sum: " + (a+d));
+        System.out.println("The sum: " + (a+f));
     }
 }
