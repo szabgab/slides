a35e64a7ed50b2176109aa703294ff81b535358c
Gabor Szabo <gabor@szabgab.com>
2025-02-13 11:43:11 +0100

    add test cases

--- python/examples/loops/test_random_loop.py
+++ python/examples/loops/test_random_loop.py
@@ -23,3 +23,50 @@ def test_random_loop(capsys, seed):
     assert out_complex == out_true
 
     print(out_true)
+
+def test_newest_random_loop_0(capsys):
+    expected = """0
+12
+25
+26
+34
+50
+65
+77
+done
+"""
+    random.seed(0)
+    while_true.random_loop()
+    out_true, _ = capsys.readouterr()
+    assert out_true == expected
+
+def test_newest_random_loop_7(capsys):
+    expected = """0
+10
+14
+26
+27
+29
+46
+49
+60
+78
+79
+95
+101
+102
+104
+117
+130
+132
+139
+141
+158
+done
+"""
+    random.seed(7)
+    while_true.random_loop()
+    out_true, _ = capsys.readouterr()
+    assert out_true == expected
+
+
