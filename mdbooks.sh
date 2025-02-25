d2d4735a3eabd5c996ee3891507f30101f63ca0e
Gabor Szabo <gabor@szabgab.com>
2025-02-25 11:44:58 +0200

    typescript

--- mdbooks.sh
+++ mdbooks.sh
@@ -3,3 +3,4 @@ mdbook build --dest-dir ../html/github-pages github-pages/
 mdbook build --dest-dir ../html/gitlab gitlab/
 mdbook build --dest-dir ../html/c c/
 mdbook build --dest-dir ../html/react react/
+mdbook build --dest-dir ../html/typescipt typescipt/
