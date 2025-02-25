0a425f6d7aec6a6e97274f0f1122c47ade86d340
Gabor Szabo <gabor@szabgab.com>
2025-02-25 11:38:00 +0200

    mdbooks

--- mdbooks.sh
+++ mdbooks.sh
@@ -1,2 +1,5 @@
 
+mdbook build --dest-dir ../html/github-pages github-pages/
+mdbook build --dest-dir ../html/gitlab gitlab/
+mdbook build --dest-dir ../html/c c/
 mdbook build --dest-dir ../html/react react/
