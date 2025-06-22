6c17da962b996c0b2fce2fc11c567fbdff4a54ad
Gabor Szabo <gabor@szabgab.com>
2025-06-22 18:13:42 +0300

    update

--- python/examples/pil/resize_image_demo.py
+++ python/examples/pil/resize_image_demo.py
@@ -8,7 +8,8 @@ in_file = sys.argv[1]
 change = float(sys.argv[2])
 out_file = sys.argv[3] if len(sys.argv) == 4 else None
 
-img = Image.open(in_file)
+img = Image.open(in_file) # opening file and reading meta
+
 print(img.size)    # a tuple
 print(img.size[0]) # width
 print(img.size[1]) # height
@@ -16,8 +17,14 @@ print(img.size[1]) # height
 width = int(change * img.size[0] / 100)
 height = int(change * img.size[1] / 100)
 
+
 out = img.resize((width, height))
+#print("image size: ", sys.getsizeof(list(img.im)))
+print("image size: ", sys.getsizeof(img.getdata()))
+print("image size: ", sys.getsizeof(img.im))
+
 out.show()
+print("image size: {}", sys.getsizeof(out.im))
 
 if out_file:
     out.save(out_file)
--- python/examples/process/process.py
+++ python/examples/process/process.py
@@ -1,8 +1,11 @@
 import time
 import sys
+import os
 
 if len(sys.argv) != 3:
     exit(f"{sys.argv[0]} SECONDS EXIT_CODE")
+
+print(f"process ID: {os.getpid()}  parent ID: {os.getppid()}")
 
 seconds = int(sys.argv[1])
 exit_code = int(sys.argv[2])
