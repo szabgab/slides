3d0a9df1cbba6de4e1f18b2bc0777e60ecfbc574
Gabor Szabo <gabor@szabgab.com>
2025-06-29 17:33:22 +0300

    generate Book.txt for leanpub

--- mdbooks.pl
+++ mdbooks.pl
@@ -129,17 +129,44 @@ sub generate_md_books {
         say "mdbook '$name'";
         say `pwd`;
 
-        my $cmd = "mdbook build";
+        my $cmd = "rm -rf book";
+        say $cmd;
+        system $cmd;
+
+        $cmd = "mdbook build";
+        say $cmd;
+        system $cmd;
+
+        convert_summary_to_book_txt();
+
+        $cmd = "rm -rf ../html/$name";
         say $cmd;
         system $cmd;
 
         $cmd = "mv book/html ../html/$name";
         say $cmd;
         system $cmd;
+
         chdir "..";
     }
 }
 
+
+sub convert_summary_to_book_txt {
+    open my $fh, "<", "src/SUMMARY.md" or die;
+    my @rows = <$fh>;
+    chomp @rows;
+    #print Dumper \@rows;
+    @rows = grep { /\(.+\)/ } @rows; # at least one character between to exclude placeholders
+    @rows = map { s{^.*\(\./}{}; $_ } @rows;
+    @rows = map { s{\).*}{}; $_ } @rows;
+    #print Dumper \@rows;
+    open my $fh, ">", "book/markdown/Book.txt" or die;
+    for my $row (@rows) {
+        print $fh "$row\n";
+    }
+    #exit;
+}
 
 sub get_title {
     my ($name) = @_;
